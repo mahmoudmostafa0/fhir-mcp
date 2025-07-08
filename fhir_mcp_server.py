e917e4344767036b40505dd3da0023f628952bd7
mahmoudmostafa0 <10292198+mahmoudmostafa0@users.noreply.github.com>
2025-07-08 15:46:17 +0300

    chore: update server config to use streamable-http transport and add MCP test server URL

--- fhir_mcp_server.py
+++ fhir_mcp_server.py
@@ -476,8 +476,8 @@ async def search_medicines(
 # ──────────────────────────────
 if __name__ == "__main__":
     mcp.run(
-        transport="sse",     # streamable-http
+        transport="streamable-http",     # streamable-http
         # host="0.0.0.0",
         # port=8080,            # choose any free port
-        mount_path="/mcp/",         # optional – default is /mcp/
+        # mount_path="/mcp/",         # optional – default is /mcp/
     )
